be115d75e77407f6d0342183b2ee05634c88c835
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-04-03 08:56:24 -0300

    Downgrade da versão do OneSignal para a versão mais estável

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { OneSignal } from 'react-native-onesignal';
+import OneSignal from 'react-native-onesignal';
 
-OneSignal.initialize('2ca27a7f-66f9-4a9b-b832-a42cf0d6744e')
+OneSignal.setAppId('2ca27a7f-66f9-4a9b-b832-a42cf0d6744e')
 
 import { Routes } from './src/routes';
 
