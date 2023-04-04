721c5cf3bd56b96cb5f8419ba34b49a7dce8e71f
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-04-04 08:27:20 -0300

    Atualiza OneSignal App ID

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import OneSignal from 'react-native-onesignal';
 
-OneSignal.setAppId('2ca27a7f-66f9-4a9b-b832-a42cf0d6744e')
+OneSignal.setAppId('981a2f30-12d9-4f28-9e81-0cc3bd29c4f0')
 
 import { Routes } from './src/routes';
 
