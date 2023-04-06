4c243030795739d695f80ba4d4f56fc8bb8ebdb6
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2023-04-06 08:08:53 -0300

    Utilizando tags para identificar o usuário

--- App.tsx
+++ App.tsx
@@ -3,18 +3,21 @@ import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import OneSignal from 'react-native-onesignal';
 
-OneSignal.setAppId('87f5490d-7b83-49ce-9432-16b1769d1bf5')
-OneSignal.setEmail('carloseduardoalvesgodoi@hotmail.com')
-
 import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
+import { tagUserEmailCreate } from './src/notifications/notificationsTags'
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
+OneSignal.setAppId('87f5490d-7b83-49ce-9432-16b1769d1bf5')
+// OneSignal.setEmail('carloseduardoalvesgodoi@hotmail.com')
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserEmailCreate('carloseduardoalvesgodoi@hotmail.com');
 
   return (
     <NativeBaseProvider theme={THEME}>
